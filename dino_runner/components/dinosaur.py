6ce312835cb9bbb87faf90863226cc8d91da3069
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2022-10-25 21:25:32 -0300

    Classe 1 - Dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -7,6 +7,7 @@ Y_POS = 310
 Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
+
 class Dinosaur:
     def __init__(self):
         self.image = RUNNING[0]
@@ -27,23 +28,21 @@ class Dinosaur:
         elif self.dino_duck:
             self.duck()
 
-
-        if self.step_index >= 10:
-            self.step_index = 0
-
-
-        if user_input[pygame.K_w] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif user_input[pygame.K_s] and not self.dino_jump:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
             self.dino_jump = False
-        elif not (self.dino_jump or user_input[pygame.K_DOWN]):
+        elif not (self.dino_jump and not self.dino_duck):
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
+
+        if self.step_index >= 10:
+            self.step_index = 0
 
     def run(self):   #essa função dar sensação que o dino esta movimentando o passo
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -52,9 +51,6 @@ class Dinosaur:
         self.dino_rect.y = Y_POS
         self.step_index += 1
      
-    def draw(self, screen):  #desenha o objeto na tela, no caso, o dinossauro.
-        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-
     def jump(self):
         self.image = JUMPING
         if self.dino_jump:
@@ -66,13 +62,13 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        self.image = DUCKING
-        if self.dino_duck:
-            self.dino_rect = self.image.get_rect() #Ja que ira modificar a posição dinamica do eixo y é necessario atualizar as dimenões novamente.
-            self.dino_rect.x = X_POS    
-            self.dino_rect.y = Y_POS_DUCK #A dimensão do eixo Y se altera quando o dino abaixa
-            self.step_index += 1
-        elif not self.dino_duck:
-            self.dino_duck = False
+        self.image = JUMPING
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect() #Ja que ira modificar a posição dinamica do eixo y é necessario atualizar as dimenões novamente.
+        self.dino_rect.x = X_POS    
+        self.dino_rect.y = Y_POS_DUCK #A dimensão do eixo Y se altera quando o dino abaixa
+        self.step_index += 1
+        self.dino_duck = False
       
-        
+    def draw(self, screen):  #desenha o objeto na tela, no caso, o dinossauro.
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -8,23 +8,23 @@ from dino_runner.components.dinosaur import Dinosaur
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption(TITLE)
-        pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.playing = False
-        self.game_speed = 20
-        self.x_pos_bg = 0
-        self.y_pos_bg = 380
+        pygame.display.set_caption(TITLE)   #titulo da janela do game
+        pygame.display.set_icon(ICON)       #icone da janela do game 
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #define as dimensões da janela do game
+        self.clock = pygame.time.Clock() #usando o metodo do pygame relogio
+        self.playing = False  
+        self.game_speed = 20  #velocidade em que o jogo ocorre
+        self.x_pos_bg = 0    #posicionamento da janela eixo x
+        self.y_pos_bg = 380  #      ''       ''   '' eixo y
         self.player = Dinosaur()
 
-    def run(self):
+    def run(self):  #metodo que executa o jogo
         # Game loop: events - update - draw
         self.playing = True
-        while self.playing:
-            self.events()
-            self.update()
-            self.draw()
+        while self.playing: 
+            self.events()  #executa o jogo
+            self.update()  #atualiza as imagens do game
+            self.draw()    #desenha no bg
         pygame.quit()
 
     def events(self):
@@ -41,8 +41,8 @@ class Game:
         self.screen.fill((255, 255, 255)) # #ffffff
         self.draw_background()
         self.player.draw(self.screen)
-        pygame.display.update()
-        pygame.display.flip()
+        pygame.display.update()  #desenha na tele que esta em constante atualização
+        pygame.display.flip()    #ordena oque vai ser desenhado na tela
 
     def draw_background(self):
         image_width = BG.get_width()
